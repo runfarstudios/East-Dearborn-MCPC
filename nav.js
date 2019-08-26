a78b7edee8ef757f91b95d70b731f1ef547ff1c8
Tailai Wang <tailaiwang01@gmail.com>
2019-08-26 18:21:58 -0400

    2019-08-26

--- nav.js
+++ nav.js
@@ -1,18 +1,24 @@
 window.onload = function(){
     window.onscroll = function(){
-        navFunction()
+        navFunction();
     }
 }
 
-var navbar = document.getElementById("nav");
+var navbar = document.getElementById("navbar");
 var gallery = document.getElementById("gallery");
 var locked = gallery.offsetTop;
 
 function navFunction(){
     if (window.pageYOffset >= locked) {
-        navbar.classList.add("locked")
+        navbar.classList.remove("nav-hidden");
+        navbar.classList.add("nav-locked");
+        gallery.classList.remove("content-idle");
+        gallery.classList.add("content-active");
       } 
     else {
-        navbar.classList.remove("locked");
+        navbar.classList.remove("nav-locked");
+        navbar.classList.add("nav-hidden");
+        gallery.classList.remove("content-active");
+        gallery.classList.add("content-idle");
     }
 }
